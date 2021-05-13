1fc1ca0bbedc5e2d3a52a5f37cd75ff34af17d85
hritesh0027 <48084818+hritesh0027@users.noreply.github.com>
2021-05-13 10:16:40 +0530

    rankedGradeBook_complete

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,13 +22,26 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
-            else
+
+            var number = Students.Count/5;
+            var sortedGrade = Students.OrderByDescending(x=>x.AverageGrade).Select(x=>x.AverageGrade).ToList();
+            if(averageGrade>=sortedGrade[ number-1 ])
             {
-                var number = Students.Count;
-                number = number/5;
                 return 'A';
             }
-            
+            if(averageGrade>=sortedGrade[ 2*number -1 ])
+            {
+                return 'B';
+            }
+            if(averageGrade>=sortedGrade[ 3*number -1 ])
+            {
+               return 'C';
+            }
+            if(averageGrade>=sortedGrade[ 4*number -1 ])
+            {    
+                return 'D';
+            }
+            return 'F';
         }
     }
 }
