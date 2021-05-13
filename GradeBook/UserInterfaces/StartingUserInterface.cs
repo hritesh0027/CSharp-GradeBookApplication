5ac4a0d9d6f2532a3ce5fbf4eb325e6503bfe804
hritesh0027 <48084818+hritesh0027@users.noreply.github.com>
2021-05-13 10:54:16 +0530

    AbstractClassImplemented

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,7 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook;
             var type = parts[2];
             if(type == "standard")
             {
